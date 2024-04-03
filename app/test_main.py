b2fae5ffb2fe40ae66f1a378dd301d57bee2fcd0
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-04-03 12:05:43 +0200

    tests for get coin

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,16 @@
 from app.main import get_coin_combination
 
+import pytest
 
-# write your tests here
+
+class TestCorrectData:
+    @pytest.mark.parametrize(
+        "coins, expected",
+        [
+            (1, [1, 0, 0, 0]),
+            (6, [1, 1, 0, 0]),
+            (17, [2, 1, 1, 0]),
+        ]
+    )
+    def test_get_reversed_list(self, coins: int, expected: list) -> None:
+        assert get_coin_combination(coins) == expected
